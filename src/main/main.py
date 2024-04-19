cabbb7aec349c904e144e171268161aa86b19482
GaborToth103 <gabor.toth.103e@gmail.com>
2024-04-19 14:16:43 +0200

    Implemented Llama3

--- src/main/main.py
+++ src/main/main.py
@@ -3,17 +3,18 @@ from mylogger import MyLogger
 import mymodels as mymodels
 import sys
 
-
 class Evaluate:
     def __init__(self) -> None:
-        self.model_llama = mymodels.ModelTapas() # FIXME modified from LLama
+        self.model_llama = mymodels.ModelLlama()
         self.models = [self.model_llama]
 
-    def scoring_answers(self, answer: str, llama_answer: str, model: mymodels.Model):
-        answer: str = str(answer.lower().strip())
-        if answer in llama_answer or llama_answer in answer:
-            if answer in llama_answer:
+    def scoring_answers(self, truth: str, model_answer: str, model: mymodels.Model):
+        truth: str = str(truth.lower().strip())
+        model_answer: str = str(model_answer.lower().strip())
+        if truth in model_answer or model_answer in truth:
+            if truth in model_answer:
                 model.score[0] += 1
+                model.score[1] += 1
             model.score[1] += 1
             log.debug(f'Llama score updated: {self.model_llama.score}')
     
@@ -35,26 +36,19 @@ class Evaluate:
                         print("yay")
                     evaluated_count += 1
                     self.scoring_answers(answer, answer, model)
-                    self.print_data(evaluated_count)
-                    if limit and evaluated_count > limit:
+                    log.info(f"Scores from {evaluated_count}: llama({(self.model_llama.score[0] * 100 / evaluated_count):.0f}%)")
+                    if limit and evaluated_count >= limit:
                         break
                 except Exception as e:
                     log.warning(e)
         return evaluated_count
-
-    def print_data(self, total):
-        message = f"scores from {total}: llama({(self.model_llama.score[0] * 100 / total):.0f}%)"
-        print(message)
-        log.info(message)
-     
 
 if __name__ == "__main__":
     log = MyLogger()
     limit = 10
     if len(sys.argv) != 1 and sys.argv[-1].isdigit:
         limit = int(sys.argv[-1])
-    log.info(f"Script tarted, limit set to {limit}")
+    log.info(f"Script started, limit set to {limit}")
     eva = Evaluate()
     mydatabase = MyDatabase()
     total = eva.evaluate(mydatabase, limit=limit)
-    eva.print_data(total)
--- src/main/mymodels.py
+++ src/main/mymodels.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForTableQuestionAnswering, TapexTokenizer, BartForConditionalGeneration, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForTableQuestionAnswering, BartForConditionalGeneration, AutoModelForSeq2SeqLM
 import urllib.request
 from llama_cpp import Llama
 import os
@@ -19,9 +19,9 @@ class Model:
 
 class ModelLlama(Model):
     def __init__(self,
-                 url: str = "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf",
+                 url: str = "https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf",
                  model_folder_path: str = 'models/',
-                 context_length: int = 2048) -> None:
+                 context_length: int = 8192) -> None:
         super().__init__()
         self.name: str = url.split("/")[-1]
         self.model_path: str = self.download_file(url, model_folder_path, self.name)
@@ -44,19 +44,9 @@ class ModelLlama(Model):
         else:
             print("File already exists.")
         return path
-    
-    def get_llm(self, context_length = 512, gpu_layers = 9999, cpu_threads=None):
-        # Creates a Llama object
-        llm = Llama(
-            model_path=self.model_path,
-            n_ctx=context_length, # context length to feed the model, the larger context length significantly increases time cost
-            n_threads=cpu_threads, # maximum number of cpu threads to use
-            n_gpu_layers=gpu_layers, # gpu layers to use if acceleration is available
-        )
-        return llm
 
     def generate_text(self, table: pd.DataFrame, question: str) -> str:
-        prompt = f"[INST] {table}\n{question}\nAnswer in one word: [/INST]"
+        prompt = f"{table}\n{question}\nAnswer in one word:"
         output = self.model(
             prompt,
             max_tokens=2048,
@@ -154,10 +144,9 @@ class ModelTranslate(Model):
         return question
     
 if __name__ == "__main__":
-    print(ModelTranslate().translate_sentence("Where's the bus stop?", target="hu"))
     print(
-        ModelTapex().generate_text(
+        ModelLlama().generate_text(
             pd.DataFrame(),
-            "Is this an empty table?"
+            "How many planets are there in our Solar System?"
         )
     )
